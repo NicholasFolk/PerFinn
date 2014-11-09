e16c9e1b4346f7088ec6cd09684e3c2f9aff629a
Nicholas Folk <nicholasfolk@gmail.com>
2014-11-08 16:07:38 -0800

    Add functionality for all Interest Calc combos
    all combos of contributions vs compounded are functional. Also
    refactored for some abstraction. For future, try to abstract the Cont
    objects.

--- javascriptFiles/InterestCalculator.js
+++ javascriptFiles/InterestCalculator.js
@@ -1,96 +1,157 @@
-
+/**
+ *
+ */
 var interestInputs = {
     initial: 0,
     contFrequency: "yearly",
     yearlyContValue: 0,
     monthlyContValue: 0,
-    compounded: "simple",
+    compounded: yearlyCont,
     timeLength: 0,
     interestRate: 0
 }
 
-function updateInterestInputs(){
-        for (var prop in interestInputs) {
-            if (interestInputs.hasOwnProperty(prop)&& typeof interestInputs[prop] === "number" && document.getElementById(prop) != null){
-                if (document.getElementById(prop).value) {
-                    interestInputs[prop] = parseInput(document.getElementById(prop).value);
-                } else {
-                    interestInputs[prop] = 0;
-                }
-            }
-        }
-    interestInputs["compounded"] = document.getElementById("compounded").value;
-    interestInputs["contFrequency"] = document.getElementById("contFrequency").value;
-
-}
-
-function updateResponseField(interest){
-    document.getElementById("response").innerHTML  = "$" + interest.toFixed(2);
-}
-
-function interestYearlyContYearlyComp(init, cont,perc,years) {
-
-    var interestCalc = function(cont,perc,years) {
-        if (years === 0) {
-            return cont;
-        } else {
-            return (cont * ((Math.pow(perc, years)))) + interestCalc(cont, perc, (years - 1));
-        }
-    }
-    if (years === 0) return init;
-    return (init * ((Math.pow(perc, years)))) + interestCalc(cont, perc, years-1);
-}
-
-function interestYearlyContMonthlyComp(init, cont,perc,years) {
-
-    var interestCalcYM = function(cont,perc,years) {
-        if (years === 0) {
-            return cont;
-        } else {
-            var newPercent = (((perc-1)/12 )+1);
-            return (cont * ((Math.pow(newPercent, years*12)))) + interestCalcYM(cont, perc, (years - 1));
-        }
-    }
-    if (years === 0) return init;
-    var newPercent = (((perc-1)/12 )+1);
-    return (init * ((Math.pow(newPercent, years*12)))) + interestCalcYM(cont, perc, years-1);
-}
-
-
-function calculateYearlyContributionsInterest() {
-    var init = interestInputs["initial"];
-    var years = interestInputs["timeLength"];
-    var percent = interestInputs["interestRate"]/100 + 1;
-    var cont = interestInputs["yearlyContValue"];
-    var result;
-    if (interestInputs["compounded"] === "yearly") {
-        result = interestYearlyContYearlyComp(init,cont, percent, years);
-    } else if (interestInputs["compounded"] === "monthly"){
-        result = interestYearlyContMonthlyComp(init,cont, percent, years);
-    } else if (interestInputs["compounded"] === "simple"){
-        result = calculateMonthlyContributionsInterest();
-    }
-
-    return result;
-}
-function calculateMonthlyContributionsInterest() {
-
-}
-function calculateNoContributionsInterest() {
-
-}
 function calculateInterest(){
     updateInterestInputs();
+    var init = interestInputs["initial"];
+    var years = Math.floor(interestInputs["timeLength"]);
+    var percent = interestInputs["interestRate"]/100 + 1;
+    var cont;
     var result;
     var frequency = interestInputs["contFrequency"];
     if (frequency === "yearly") {
-        result = calculateYearlyContributionsInterest();
+        cont = interestInputs["yearlyContValue"];
+        result = calculateCompoundInterest(init,cont, percent, years, yearlyCont);
     } else if (frequency === "monthly"){
-        result = calculateMonthlyContributionsInterest();
+        cont = interestInputs["monthlyContValue"];
+        result = calculateCompoundInterest(init,cont, percent, years*12, monthlyCont);
     } else if (frequency === "never"){
-        result = calculateNoContributionsInterest();
+        cont = 0;
+        result = calculateCompoundInterest(init,cont, percent, years, neverCont);
     }
     updateResponseField(result);
+}
+
+function calculateCompoundInterest(init,cont, percent, time, freqObj) {
+    var result;
+    var compounded = interestInputs["compounded"];
+    if (compounded === "yearly") {
+        result = freqObj.yearlyComp(init, cont, percent, time);
+    } else if (compounded === "monthly"){
+        result = freqObj.monthlyComp(init, cont, percent, time);
+    } else if (compounded === "simple"){
+        result = freqObj.simple(init, cont, percent, time);
+    }
+    return result;
+}
+
+var yearlyCont = {
+    yearlyComp: function(init, cont,perc,years) {
+        var interestCalc = function(cont,perc,years) {
+            if (years === 0) {
+                return cont;
+            } else {
+                return (cont * ((Math.pow(perc, years)))) + interestCalc(cont, perc, (years - 1));
+            }
+        }
+        if (years === 0) return init;
+        return (init * ((Math.pow(perc, years)))) + interestCalc(cont, perc, years-1);
+    },
+
+    monthlyComp: function(init, cont,perc,years) {
+        var interestCalcYM = function(cont,perc,years) {
+            if (years === 0) {
+                return cont;
+            } else {
+                return (cont * ((Math.pow(perc, years*12)))) + interestCalcYM(cont, perc, (years - 1));
+            }
+        }
+        if (years === 0) return init;
+        var newPercent = (((perc-1)/12 )+1);
+        return (init * ((Math.pow(newPercent, years*12)))) + interestCalcYM(cont, newPercent, years-1);
+    },
+
+
+    simple: function(init,cont, percent, years) {
+        var rawPercent = percent - 1;
+        var result = (init + (init * rawPercent * years));
+        years--;
+        while (years >= 0) {
+            result += (cont + (cont * rawPercent * years));
+            years--;
+        }
+        return result;
+    }
+}
+
+var monthlyCont = {
+    yearlyComp: function(init, cont,perc,months) {
+        var interestCalc = function(cont,perc,months) {
+            if (months === 0) {
+                return cont;
+            } else {
+                return (cont * ((Math.pow(perc, months/12)))) + interestCalc(cont, perc, (months - 1));
+            }
+        }
+        if (months === 0) return init;
+        return (init * ((Math.pow(perc, months/12)))) + interestCalc(cont, perc, months-1);
+    },
+
+    monthlyComp: function(init, cont,perc,months) {
+        var interestCalcYM = function(cont,perc,months) {
+            if (months === 0) {
+                return cont;
+            } else {
+                return (cont * ((Math.pow(perc, months)))) + interestCalcYM(cont, perc, (months - 1));
+            }
+        }
+        if (months === 0) return init;
+        var newPercent = (((perc-1)/12 )+1);
+        return (init * ((Math.pow(newPercent, months)))) + interestCalcYM(cont, newPercent, months-1);
+    },
+
+    simple: function(init,cont, percent, months) {
+        var rawPercent = (percent - 1)/12;
+        var result = (init + (init * rawPercent * months));
+        months--;
+        while (months >= 0) {
+            result += (cont + (cont * rawPercent * months));
+            months--;
+        }
+        return result;
+    }
+}
+
+var neverCont = {
+    yearlyComp: function(init, cont,perc,years) {
+        return (init * ((Math.pow(perc, years))));
+    },
+
+    monthlyComp: function(init, cont,perc,years) {
+        var newPercent = (((perc-1)/12 )+1);
+        return (init * ((Math.pow(newPercent, years*12))));
+    },
+
+    simple: function(init,cont, percent, years) {
+        var rawPercent = percent - 1;
+        var result = (init + (init * rawPercent * years));
+        return result;
+    }
+}
+
+function updateInterestInputs(){
+    for (var prop in interestInputs) {
+        if (interestInputs.hasOwnProperty(prop)&& typeof interestInputs[prop] === "number" && document.getElementById(prop) != null){
+            if (document.getElementById(prop).value) {
+                interestInputs[prop] = parseInput(document.getElementById(prop).value);
+            } else {
+                interestInputs[prop] = 0;
+            }
+        }
+    }
+    interestInputs["compounded"] = document.getElementById("compounded").value;
+    interestInputs["contFrequency"] = document.getElementById("contFrequency").value;
+
 }
 
 function changeFrequency(){
@@ -107,5 +168,22 @@ function changeFrequency(){
 
     } else if (valueOf == "never"){
         changeDisplay('frequency','none');
+    }
+}
+
+function updateResponseField(interest){
+    document.getElementById("response").innerHTML  = "The total amount accrued over " + interestInputs['timeLength'] + " years would be " +
+        "<span class='blueBolded'>$" + interest.toFixed(2) + "</span>. Note that this value " +
+        "includes your final contribution.";
+}
+
+function getFrequencyObject(){
+    var frequencyString = document.getElementById("contFrequency").value;
+    if (frequencyString === "yearly"){
+        return yearlyCont;
+    } else if (frequencyString === "monthly"){
+        return monthlyCont;
+    } if (frequencyString === "never"){
+        return neverCont;
     }
 }
